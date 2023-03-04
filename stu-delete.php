2cd0ae4df6809e0d576e4a3c3d8c84e256d90900
mr-bhanushali03 <mrbhanushali03@gmail.com>
2023-03-04 18:27:28 +0530

    delete record with relationship tables

--- stu-delete.php
+++ stu-delete.php
@@ -3,8 +3,20 @@ include('connection.php');
     // Delete Query For Student Table
     if(isset($_GET['hidden_id'])){
         $student_id = $_GET['hidden_id'];
-        $delete = "DELETE FROM students WHERE id=$student_id";
-        $con = mysqli_query($con,$delete);
+
+        $division_id = "SELECT id FROM divisions where student_id=$student_id";
+        $div_id_result = mysqli_query($con,$division_id);
+        $row = mysqli_fetch_assoc($div_id_result);
+
+        $teacher_divisions = "DELETE FROM teacher_divisions WHERE division_id=$row[id]";
+        mysqli_query($con,$teacher_divisions);
+
+        $division = "DELETE FROM divisions WHERE student_id=$student_id";
+        mysqli_query($con,$division);
+
+        $student = "DELETE FROM students WHERE id=$student_id";
+        mysqli_query($con,$student);
+
         header('location:student.php');
     }
 ?>
--- teacher-delete.php
+++ teacher-delete.php
@@ -2,9 +2,21 @@
 include('connection.php');
     // Delete Query For Student Table
     if(isset($_GET['hidden_id'])){
-        $student_id = $_GET['hidden_id'];
-        $delete = "DELETE FROM teachers WHERE id=$student_id";
-        $con = mysqli_query($con,$delete);
+        $teacher_id = $_GET['hidden_id'];
+
+        $division_id = "SELECT id FROM divisions where teacher_id=$teacher_id";
+        $div_id_result = mysqli_query($con,$division_id);
+        $row = mysqli_fetch_assoc($div_id_result);
+
+        $teacher_divisions = "DELETE FROM teacher_divisions WHERE division_id=$row[id]";
+        mysqli_query($con,$teacher_divisions);
+
+        $division = "DELETE FROM divisions WHERE teacher_id=$teacher_id";
+        mysqli_query($con,$division);
+
+        $teacher = "DELETE FROM teachers WHERE id=$teacher_id";
+        mysqli_query($con,$teacher);
+
         header('location:teacher.php');
     }
 ?>
